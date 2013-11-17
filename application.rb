b6d478203fa9e37716862cfa36662e5c6dd3616e
Jason Wan <jwan823@gmail.com>
2013-11-16 17:12:41 -0800

    Added contact creation feature

--- application.rb
+++ application.rb
@@ -9,10 +9,29 @@ class Application
     loop do
 		  show_main_menu
       input = clean_input(gets)
+      manage_input(input)
       break if input == "quit"
     end
     show_exit_msg
 	end
+
+  def manage_input(input)
+    case input.downcase
+    when "new"
+      create_contact
+    end
+  end
+
+  def create_contact
+    print "Enter your first name: "
+    first_name = clean_input(gets)
+    print "Enter your last name: "
+    last_name = clean_input(gets)
+    print "Enter your email: "
+    email = clean_input(gets)
+    full_name = cap_first_letter(first_name) + " " + cap_first_letter(last_name)
+    @contacts << Contact.new(full_name, email) unless full_name.empty? || email.empty?
+  end
 
   def show_intro
     puts "Welcome to the app. What's next?"
@@ -31,7 +50,11 @@ class Application
   end
 
   def clean_input(input)
-    input.chomp.rstrip.downcase
+    input.chomp.rstrip
   end
 
+  def cap_first_letter(input)
+    input[0].upcase + input[1..-1] unless input.empty?
+    input ||= ""
+  end
 end
--- contact.rb
+++ contact.rb
@@ -3,11 +3,18 @@ class Contact
   attr_accessor :first_name, :last_name, :email
 
   def initialize(name, email)
-
+    split_name(name)
+    @email = email
   end
 
   def to_s
+    "#{@first_name} #{@last_name} (#{@email})"
+  end
 
+  def split_name(name)
+    name = name.split(" ")
+    @first_name = name.first
+    @last_name = name.last
   end
 
 end
