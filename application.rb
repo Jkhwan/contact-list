110459bba6fc7c675828e7d3d1b49fc166d572ce
Jason Wan <jwan823@gmail.com>
2013-11-16 16:47:15 -0800

    Added menu loop and quit feature

--- application.rb
+++ application.rb
@@ -5,16 +5,33 @@ class Application
 	end
 
 	def run
-		show_main_menu
-		input = gets.chomp
+    show_intro
+    loop do
+		  show_main_menu
+      input = clean_input(gets)
+      break if input == "quit"
+    end
+    show_exit_msg
 	end
 
+  def show_intro
+    puts "Welcome to the app. What's next?"
+  end
+
+  def show_exit_msg
+    puts "Adeu!"
+  end
+
 	def show_main_menu
-		puts "Welcome to the app. What's next?"
 		puts " new      - Create a new contact"
     puts " list     - List all contacts"
     puts " show :id - Display contact details"
+    puts " quit     - Quit the program"
     print "> "
   end
-  
+
+  def clean_input(input)
+    input.chomp.rstrip.downcase
+  end
+
 end
